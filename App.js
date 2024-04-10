119d8a00f0ef3032281f35078ccc1d07da7e2d6a
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-10 22:01:20 +0200

    [feat] Add Home

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+      <Text style={{
+          color: '#FDFCFE',
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 48
+        }}>
+        Cooking class 101
+      </Text>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Text style={{
+        color: '#6B6B6B',
+        fontSize: 16
+      }}>
+        Friday, April, 10th, 2024.
+      </Text>
+    </View>
+  )
+}
